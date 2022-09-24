79c0348d9d7b93409b003ee00ba079fd87466152
gowithmanh <91020595+gowithmanh@users.noreply.github.com>
2022-09-24 21:23:13 +0700

    Code

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Entre your username");
+ var b=prompt("Enter password:");
+  	if(a== "Miss Hanh" && b=="24092022")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
